4e9c06183066d41f7b20adda0ac5d8a8db26b082
Ezequiel <crespo.ep@gmail.com>
2022-07-18 22:16:57 -0300

    Lotteries id incrementation when they are created

--- smartcontracts/contracts/Lottery.sol
+++ smartcontracts/contracts/Lottery.sol
@@ -1,4 +1,13 @@
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/utils/Counters.sol";
+
 contract Lottery {
+  using Counters for Counters.Counter;
+
+  Counters.Counter public lotteryId;
+
+  function createLottery() public {
+    lotteryId.increment();
+  }
 }
--- smartcontracts/test/Lottery.test.js
+++ smartcontracts/test/Lottery.test.js
@@ -13,4 +13,18 @@ describe('Lottery', () => {
     expect(address).not.to.equal("");
     expect(address).not.to.equal(undefined);
   });
+
+  it('game creation increments the number of games', async () => {
+    await deployments.fixture(["test"]);
+    let Lottery = await deployments.get("Lottery");
+    let lottery = await ethers.getContractAt("Lottery", Lottery.address);
+
+    let num = await lottery.lotteryId();
+    expect(num).to.equal(0);
+
+    await lottery.createLottery();
+
+    num = await lottery.lotteryId();
+    expect(num).to.equal(1);
+  });
 })
