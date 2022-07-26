73429e0f2d0aa3ae69d48e9684b24fc5033a4e53
Ezequiel <crespo.ep@gmail.com>
2022-07-26 17:43:03 -0300

    progress so far

--- frontend/src/components/LotteryList.js
+++ frontend/src/components/LotteryList.js
@@ -1,6 +1,35 @@
 import {lotteries} from "../lotteries";
+import {useEffect, useState} from "react";
+import { ethers } from "ethers";
+import contractAddress from "../contracts/contract-address.json";
+import LotteryArtifact from "../contracts/LotteryGame.json";
 
 const LotteryList = () => {
+  const [ lotteryContract, setLotteryContract ] = useState(null)
+  const [ lotteries, setLotteries ] = useState([])
+
+  useEffect(() => {
+    const { ethereum } = window;
+    const provider = new ethers.providers.Web3Provider(ethereum);
+
+    const lotteryContract = new ethers.Contract(
+      contractAddress.Lottery,
+      LotteryArtifact.abi,
+      provider.getSigner(0)
+    )
+
+    setLotteryContract(lotteryContract)
+  }, [])
+
+  useEffect(() => {
+    const getNumber = async () => {
+      if (lotteryContract) {
+        const lotteries = await lotteryContract.getOpenLotteriesIds()
+        setLotteries(lotteries)
+      }
+    }
+    getNumber()
+  }, [lotteryContract])
 
   return (
     <div>
