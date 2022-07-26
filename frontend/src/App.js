37736c1a84a50d757210d86eba92da27e90dd7ac
Ezequiel <crespo.ep@gmail.com>
2022-07-26 19:41:21 -0300

    balance added to header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,11 @@ import './App.css';
 
 function App() {
   const [ account, setAccount ] = useState(null)
+  const [ balance, setBalance ] = useState(null)
 
   return (
     <div className="App">
-      <Header account={account} setAccount={setAccount} />
+      <Header account={account} setAccount={setAccount} balance={balance} setBalance={setBalance} />
       <Outlet />
     </div>
   );
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,38 @@
+import {BigNumber, ethers} from "ethers";
 import HamburgerButton from "./HamburgerButton";
 import Navbar from "./Navbar";
 
-const Header = ({ account, setAccount }) => {
+const Header = ({
+  account,
+  setAccount,
+  balance,
+  setBalance
+}) => {
   const connectWallet = async () => {
     const { ethereum } = window;
     const [ account ] = await ethereum.request({ method: 'eth_requestAccounts' });
     setAccount(account)
 
+    const hexEthBalance = await ethereum.request({
+      method: "eth_getBalance",
+      params: [account, "latest"]
+    })
+
+    let ethBalance = ethers.utils.formatEther(BigNumber.from(hexEthBalance));
+    ethBalance = (+ethBalance).toFixed(4);
+    setBalance(ethBalance);
+
     ethereum.on("accountsChanged", async (accounts) => {
       const newAccount = accounts[0];
       setAccount(newAccount);
+
+      const newHexBalance = await ethereum.request({
+        method: 'eth_getBalance',
+        params: [newAccount, 'latest']
+      })
+      let newEthBalance = ethers.utils.formatEther(BigNumber.from(newHexBalance))
+      newEthBalance = (+newEthBalance).toFixed(4);
+      setBalance(newEthBalance)
     })
   }
 
