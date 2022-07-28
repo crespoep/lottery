dab6beba60956f52371b48f523a59e1839bb2cb4
Ezequiel <crespo.ep@gmail.com>
2022-07-28 11:05:57 -0300

    Kovan config added

--- smartcontracts/deploy/01_Deploy_LotteryGame.js
+++ smartcontracts/deploy/01_Deploy_LotteryGame.js
@@ -28,9 +28,6 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   keyHash = networkConfig[chainId].keyHash;
   fee = networkConfig[chainId].fee;
 
-  // const keyHash = "0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311"
-  // const fee = "1000000000000000000" // 0.1 LINK
-
   const lottery = await deploy("LotteryGame", {
     from: deployer,
     args: [
