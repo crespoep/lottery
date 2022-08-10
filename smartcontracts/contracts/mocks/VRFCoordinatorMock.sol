b5b8b8bd6c558dccbfbc1db5164e19625ed06e69
Ezequiel <crespo.ep@gmail.com>
2022-08-10 14:41:08 -0300

    VRF v2 first integration done

--- smartcontracts/contracts/mocks/VRFCoordinatorMock.sol
+++
@@ -1,4 +0,0 @@
-// solhint-disable-next-line
-pragma solidity ^0.8.0;
-
-import "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorMock.sol";
--- smartcontracts/deploy/00_Deploy_Mocks.js
+++ smartcontracts/deploy/00_Deploy_Mocks.js
@@ -5,18 +5,19 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deployer } = await getNamedAccounts();
   const { chainId } = network.config;
 
+  const BASE_FEE = "250000000000000000" // 0.25
+  const GAS_PRICE_LINK = 1e9
+
   if (chainId === 31337) {
     log("Using Hardhat network, deploying mocks...");
 
-    const linkTokenMock = await deploy("LinkToken", {
+    await deploy("VRFCoordinatorV2Mock", {
       from: deployer,
       log: true,
-    });
-
-    await deploy("VRFCoordinatorMock", {
-      from: deployer,
-      log: true,
-      args: [linkTokenMock.address],
+      args: [
+        BASE_FEE,
+        GAS_PRICE_LINK
+      ],
     });
 
     log("Mocks were deployed");
