2b2c09b8fe9dfe0b7e9a2cb8e6af6c8b90f76589
Ezequiel <crespo.ep@gmail.com>
2022-09-26 20:32:58 -0300

    improvements

--- smartcontracts/README.md
+++ smartcontracts/README.md
@@ -2,13 +2,9 @@
 
 ### Create new lotteries:
 ``` bash
-npx hardhat create-lottery --ticket 10000000000000000 --duration 300 --network localhost
+npx hardhat create-lottery --contract <contract-address> --ticket 10000000000000000 --duration 300 --network localhost
 ```
 
 ``` bash
-npx hardhat participate --lotteryid 1 --user <user-address> --ticket 10000000000000000
+npx hardhat participate --contract <contract-address> --lotteryid 1 --user <user-address> --ticket 10000000000000000
 ```
-
-### Testing
-The script "npm run test" will run a special configuration for mocha to suppress annoying logs that are fired in the console in the middle of tests.
-The drawback is that this command doesn't compile the contracts, so it's preferable to use hardhat test command most of the time.
--- smartcontracts/tasks/createLottery.ts
+++ smartcontracts/tasks/createLottery.ts
@@ -2,27 +2,27 @@ import {HardhatRuntimeEnvironment} from "hardhat/types";
 import { task } from "hardhat/config";
 
 task("create-lottery", "Create new lottery")
+  .addParam("contract", "LotteryGame contract address")
   .addParam("ticket", "Ticket price to participate in the lottery")
   .addParam("duration", "Lottery duration")
   .setAction(async (taskArgs, hre: HardhatRuntimeEnvironment) => {
     const ethers = hre.ethers
-
-    const Lottery = await hre.deployments.get("LotteryGame");
+    
     const lotteryContract = await hre.ethers.getContractAt(
       "LotteryGame",
-      Lottery.address
+      taskArgs.contract
     );
-
+    
     const duration = taskArgs.duration;
     const ticket = taskArgs.ticket;
-
+    
     const transaction = await lotteryContract.createLottery(
       ethers.BigNumber.from(ticket),
       parseInt(duration)
     );
-
+    
     const receipt = await transaction.wait()
-
+    
     console.log(receipt)
     console.log("Transaction Mined. Lottery created successfully");
   });
